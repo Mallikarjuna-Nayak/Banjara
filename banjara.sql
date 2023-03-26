666029eb87b1fb0753a23a01c50235d0da1acc0a
Mallikarjuna-Nayak <115221178+Mallikarjuna-Nayak@users.noreply.github.com>
2023-03-26 13:20:35 +0530

    Add files via upload

---
+++ banjara.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2023 at 08:48 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banjara`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `body_type`
+--
+-- Error reading structure for table banjara.body_type: #1932 - Table &#039;banjara.body_type&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.body_type: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`body_type`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+-- Error reading structure for table banjara.cities: #1932 - Table &#039;banjara.cities&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.cities: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`cities`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clan_lineage`
+--
+-- Error reading structure for table banjara.clan_lineage: #1932 - Table &#039;banjara.clan_lineage&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.clan_lineage: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`clan_lineage`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complexion`
+--
+-- Error reading structure for table banjara.complexion: #1932 - Table &#039;banjara.complexion&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.complexion: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`complexion`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+-- Error reading structure for table banjara.countries: #1932 - Table &#039;banjara.countries&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.countries: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`countries`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drinking_habits`
+--
+-- Error reading structure for table banjara.drinking_habits: #1932 - Table &#039;banjara.drinking_habits&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.drinking_habits: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`drinking_habits`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eating_habits`
+--
+-- Error reading structure for table banjara.eating_habits: #1932 - Table &#039;banjara.eating_habits&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.eating_habits: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`eating_habits`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education_category`
+--
+-- Error reading structure for table banjara.education_category: #1932 - Table &#039;banjara.education_category&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.education_category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`education_category`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_status`
+--
+-- Error reading structure for table banjara.family_status: #1932 - Table &#039;banjara.family_status&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.family_status: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`family_status`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_type`
+--
+-- Error reading structure for table banjara.family_type: #1932 - Table &#039;banjara.family_type&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.family_type: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`family_type`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_values`
+--
+-- Error reading structure for table banjara.family_values: #1932 - Table &#039;banjara.family_values&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.family_values: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`family_values`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+-- Error reading structure for table banjara.gender: #1932 - Table &#039;banjara.gender&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.gender: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`gender`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gotras`
+--
+-- Error reading structure for table banjara.gotras: #1932 - Table &#039;banjara.gotras&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.gotras: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`gotras`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `height`
+--
+-- Error reading structure for table banjara.height: #1932 - Table &#039;banjara.height&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.height: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`height`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inbox`
+--
+-- Error reading structure for table banjara.inbox: #1932 - Table &#039;banjara.inbox&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.inbox: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`inbox`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `income`
+--
+-- Error reading structure for table banjara.income: #1932 - Table &#039;banjara.income&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.income: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`income`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `martial_status`
+--
+-- Error reading structure for table banjara.martial_status: #1932 - Table &#039;banjara.martial_status&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.martial_status: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`martial_status`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+-- Error reading structure for table banjara.packages: #1932 - Table &#039;banjara.packages&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.packages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`packages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent_occupation`
+--
+-- Error reading structure for table banjara.parent_occupation: #1932 - Table &#039;banjara.parent_occupation&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.parent_occupation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`parent_occupation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent_type`
+--
+-- Error reading structure for table banjara.parent_type: #1932 - Table &#039;banjara.parent_type&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.parent_type: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`parent_type`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `physical_status`
+--
+-- Error reading structure for table banjara.physical_status: #1932 - Table &#039;banjara.physical_status&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.physical_status: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`physical_status`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile_for`
+--
+-- Error reading structure for table banjara.profile_for: #1932 - Table &#039;banjara.profile_for&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.profile_for: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`profile_for`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sadmin`
+--
+-- Error reading structure for table banjara.sadmin: #1932 - Table &#039;banjara.sadmin&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.sadmin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`sadmin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `smoking_habit`
+--
+-- Error reading structure for table banjara.smoking_habit: #1932 - Table &#039;banjara.smoking_habit&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.smoking_habit: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`smoking_habit`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `states`
+--
+-- Error reading structure for table banjara.states: #1932 - Table &#039;banjara.states&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.states: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`states`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table banjara.users: #1932 - Table &#039;banjara.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weight`
+--
+-- Error reading structure for table banjara.weight: #1932 - Table &#039;banjara.weight&#039; doesn&#039;t exist in engine
+-- Error reading data for table banjara.weight: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `banjara`.`weight`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
